55e4a8d44608f60f6385c919b19f31fd7998b2ec
Jonathan Oyarce <jonathan.oyarce@mail.udp.cl>
2025-10-17 15:36:03 -0300

    Cambios Grandes - StateMachine - Proyectos - Ciclo de Vida - Etc

--- Gestion_Doc/urls.py
+++ Gestion_Doc/urls.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from Usuario.views import list_files
 
-# Gestion_Doc>urls.py:
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("microsoft_auth.urls")),   # sin prefijo
+    path("", include("microsoft_auth.urls")),
     path("usuario/", include("Usuario.urls")),
+    path("documentos/", include("Gestion_Documentos_StateMachine.urls")),  # ✅ correcto
 ]
