2a2f6e75fd9ddffa8a98176a111688d9d28ce349
Jonathan Oyarce <jonathan.oyarce@mail.udp.cl>
2025-10-19 18:35:00 -0300

    Maquina de Estados - Historial

--- Gestion_Documentos_StateMachine/urls.py
+++ Gestion_Documentos_StateMachine/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
+from .views import detalle_documento
 
 urlpatterns = [
     path("mis-documentos/", views.lista_documentos_asignados, name="lista_documentos_asignados"),
     path("simulador/", views.simulador_estado, name="simulador_estado"),
+    path('documentos/<int:requerimiento_id>/', views.detalle_documento, name='detalle_documento'),
+    path('documentos/documentos/<int:requerimiento_id>/', detalle_documento, name='detalle_documento'),
+
 ]
+
